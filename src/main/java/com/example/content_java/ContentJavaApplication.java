5930c6a5896168c86cda712e2ac93846129108a6
jeongabae <gabea52@gmail.com>
2023-12-12 12:05:49 +0900

    [create] content_java

---
+++ src/main/java/com/example/content_java/ContentJavaApplication.java
@@ -0,0 +1,13 @@
+package com.example.content_java;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContentJavaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ContentJavaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/content_java/ContentJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.content_java;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContentJavaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
